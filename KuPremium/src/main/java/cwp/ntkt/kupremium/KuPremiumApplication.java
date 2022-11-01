3d7e93e7a5af6ba314d0b34636c55515ff799ffb
Itsmexander <chawish.p@ku.th>
2022-11-01 14:46:56 +0700

    27 oct 2022 0834PM

---
+++ KuPremium/src/main/java/cwp/ntkt/kupremium/KuPremiumApplication.java
@@ -0,0 +1,13 @@
+package cwp.ntkt.kupremium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuPremiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuPremiumApplication.class, args);
+    }
+
+}
