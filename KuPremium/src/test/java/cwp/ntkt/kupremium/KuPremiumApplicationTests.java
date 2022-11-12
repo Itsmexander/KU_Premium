3a6f42223f01f3822823811dad83aea1f39b7acc
Itsmexander <chawish.p@ku.th>
2022-11-12 22:46:41 +0700

    all files 12112022

---
+++ KuPremium/src/test/java/cwp/ntkt/kupremium/KuPremiumApplicationTests.java
@@ -0,0 +1,13 @@
+package cwp.ntkt.kupremium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuPremiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
