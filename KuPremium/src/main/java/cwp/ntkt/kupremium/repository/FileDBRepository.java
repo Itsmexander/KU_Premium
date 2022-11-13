d8e3116fd0b1c0493b73ec0190eece3bd7b56705
Itsmexander <chawish.p@ku.th>
2022-11-13 23:32:01 +0700

    FileStorageService 13112022

---
+++ KuPremium/src/main/java/cwp/ntkt/kupremium/repository/FileDBRepository.java
@@ -0,0 +1,12 @@
+package cwp.ntkt.kupremium.repository;
+
+import cwp.ntkt.kupremium.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
+}
